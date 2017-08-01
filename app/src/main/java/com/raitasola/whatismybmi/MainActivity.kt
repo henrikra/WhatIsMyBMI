87a164fe396668e4918887b7f2e8be2752bec991
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-08-01 23:55:07 +0300

    Add SeekBar for height

--- app/src/main/java/com/raitasola/whatismybmi/MainActivity.kt
+++ app/src/main/java/com/raitasola/whatismybmi/MainActivity.kt
@@ -2,11 +2,31 @@ package com.raitasola.whatismybmi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var heightSlider: SeekBar
+    lateinit var heightText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        heightSlider = findViewById(R.id.heightSlider) as SeekBar
+        heightText = findViewById(R.id.heightText) as TextView
+
+        heightSlider.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                heightText.text = progress.toString()
+            }
+        })
     }
 }
