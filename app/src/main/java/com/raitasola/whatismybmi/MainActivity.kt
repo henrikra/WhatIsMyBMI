9b5ac97e2d959fa9c67722f57ad631cf4f5efeb6
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-08-02 00:37:50 +0300

    Remove unused imports

--- app/src/main/java/com/raitasola/whatismybmi/MainActivity.kt
+++ app/src/main/java/com/raitasola/whatismybmi/MainActivity.kt
@@ -2,10 +2,8 @@ package com.raitasola.whatismybmi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.SeekBar
 import android.widget.TextView
-import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
