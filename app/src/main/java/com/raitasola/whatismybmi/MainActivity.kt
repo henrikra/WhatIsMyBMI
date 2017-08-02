7e5afd89f603bf8fe6a19bb6307f85c851eb9968
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-08-02 13:45:03 +0300

    Add plugin which allows cleaner access to views

--- app/src/main/java/com/raitasola/whatismybmi/MainActivity.kt
+++ app/src/main/java/com/raitasola/whatismybmi/MainActivity.kt
@@ -3,25 +3,13 @@ package com.raitasola.whatismybmi
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var heightSlider: SeekBar
-    lateinit var heightText: TextView
-    lateinit var weightSlider: SeekBar
-    lateinit var weightText: TextView
-    lateinit var bmiResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        heightSlider = findViewById(R.id.heightSlider) as SeekBar
-        heightText = findViewById(R.id.heightText) as TextView
-        weightSlider = findViewById(R.id.weightSlider) as SeekBar
-        weightText = findViewById(R.id.weightText) as TextView
-        bmiResult = findViewById(R.id.bmiResult) as TextView
 
         heightSlider.max = 240
         heightSlider.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
